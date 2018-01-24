c06c800a2dab221bf4de85ca518eff5ba2f6d4b7
lslgg <449291161@qq.com>
2018-01-24 15:05:50 +0800

    ok

--- src/app/common/common.ts
+++ src/app/common/common.ts
@@ -1,3 +1,6 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
 export class CommonData implements CommonData {
 
     startDate: String;
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -20,7 +20,7 @@
 
 /** IE9, IE10 and IE11 requires all of the following polyfills. **/
 import 'core-js/es6/symbol';
-import 'core-js/es6/object';
+import 'core-js/es7/object';
 import 'core-js/es6/function';
 import 'core-js/es6/parse-int';
 import 'core-js/es6/parse-float';
@@ -33,9 +33,10 @@ import 'core-js/es6/regexp';
 import 'core-js/es6/map';
 import 'core-js/es6/weak-map';
 import 'core-js/es6/set';
+import 'whatwg-fetch';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
+import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
