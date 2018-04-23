990a36400ceedcd8524b34bed3d4e0250c35be3e
luke123321 <437032704@qq.com>
2018-04-23 18:06:18 +0800

    ok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  dataServer: 'http://211.149.219.127:8080'
+  dataServer: 'http://localhost:4200'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: true,
-  dataServer: 'http://211.149.219.127:8080'  
+  dataServer: 'http://localhost:4200'
 };
